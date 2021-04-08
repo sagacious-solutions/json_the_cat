7e32dad9cee3b261fd167a4d0f4bc8926aa4fdc2
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-08 19:41:15 +0000

    production ready

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,78 +6,20 @@ const haltProgram = (error) => {
 };
 
 if (process.argv.length < 3)  {
-  haltProgram();
+  haltProgram(`Insuffient paramters passed.`);
 }
 
 const apiBreedSearch = `https://api.thecatapi.com/v1/breeds/search?`;
 const breed = process.argv[2];
-// console.log(breed);
+
 let searchData = {};
 
+// Make a request to the api for the breed. If found print out just the description, otherwise halt
 request(`${apiBreedSearch}q=${breed}`, (error, response, body) => {
   searchData = JSON.parse(body);
 
   if (!searchData.length) {
     haltProgram(`We didn't find that breed`);
   }
-
-
-
   console.log(searchData[0].description);
 });
-
-
-
-
-
-////////////////////////////////// HELPFUL ///////////////////////////////////
-
-
-// GET
-// /breeds/search
-// Search for a Breed by using part of it’s name as the ‘q’ query parameter.
-
-// e.g ?q=sib to search for Siberian
-// https://api.thecatapi.com/v1/breeds/search?q=sib
-
-// [
-//   {
-//     weight: { imperial: '8 - 16', metric: '4 - 7' },
-//     id: 'sibe',
-//     name: 'Siberian',
-//     cfa_url: 'http://cfa.org/Breeds/BreedsSthruT/Siberian.aspx',
-//     vetstreet_url: 'http://www.vetstreet.com/cats/siberian',
-//     vcahospitals_url: 'https://vcahospitals.com/know-your-pet/cat-breeds/siberian',
-//     temperament: 'Curious, Intelligent, Loyal, Sweet, Agile, Playful, Affectionate',
-//     origin: 'Russia',
-//     country_codes: 'RU',
-//     country_code: 'RU',
-//     description: 'The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors. ',
-//     life_span: '12 - 15',
-//     indoor: 0,
-//     lap: 1,
-//     alt_names: 'Moscow Semi-longhair, HairSiberian Forest Cat',
-//     adaptability: 5,
-//     affection_level: 5,
-//     child_friendly: 4,
-//     dog_friendly: 5,
-//     energy_level: 5,
-//     grooming: 2,
-//     health_issues: 2,
-//     intelligence: 5,
-//     shedding_level: 3,
-//     social_needs: 4,
-//     stranger_friendly: 3,
-//     vocalisation: 1,
-//     experimental: 0,
-//     hairless: 0,
-//     natural: 1,
-//     rare: 0,
-//     rex: 0,
-//     suppressed_tail: 0,
-//     short_legs: 0,
-//     wikipedia_url: 'https://en.wikipedia.org/wiki/Siberian_(cat)',
-//     hypoallergenic: 1,
-//     reference_image_id: '3bkZAjRh1'
-//   }
-// ]
