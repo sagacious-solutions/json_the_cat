fb13cacb12dc172b76cd58ffdd04daf94c5e927f
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-08 19:05:43 +0000

    getting setup

---
+++ breedFetcher.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////// HELPFUL ///////////////////////////////////
+
+
+// GET
+// /breeds/search
+// Search for a Breed by using part of it’s name as the ‘q’ query parameter.
+
+// e.g ?q=sib to search for Siberian 
+// https://api.thecatapi.com/v1/breeds/search?q=sib
