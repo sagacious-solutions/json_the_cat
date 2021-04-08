1894fbcbac304d835963d4e47dcb5a8a12d6a6d7
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-08 19:20:57 +0000

    Have data in terminal. Preparing to parse the data

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,27 @@
 const request = require('request');
 
+const haltProgram = () => {
+  console.log(`We've encountered a fatal error and had to exit.`)
+  process.exit();
+}
+
+if (process.argv.length < 3)  {
+  haltProgram();
+}
+
+const apiBreedSearch = `https://api.thecatapi.com/v1/breeds/search?`;
+const breed = process.argv[2];
 
 
 
+// console.log(breed);
+// console.log(typeof body);
 
-
-
-
-
-
-
-
-
-
+request(`${apiBreedSearch}q=${breed}`, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
 
 
 
